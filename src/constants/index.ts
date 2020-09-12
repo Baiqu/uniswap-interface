1c3ee00e390397539529f45b36f72f117ea73580
Baiqu <yz@100to.com>
2020-09-12 11:02:12 +0800

    有問題

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x0b27261444F8cec51e5f558bc32591931C8826B9'
 
 // a list of tokens by chain
 type ChainTokenList = {
